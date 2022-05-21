f8e6276c95749aaddab99b5387f66e7db099f49e
David Oniani <onianidavid@gmail.com>
2022-05-21 06:10:41 -0700

    Switched to the latest Catch2 and updated .clang-format

--- include/bf.hpp
+++ include/bf.hpp
@@ -34,31 +34,31 @@
 
 namespace bf {
 
-  /// @brief Declaration of the concept `Hashable`, which is satisfied by any type `T` such that for
-  /// values `a` of type `T`, the expression `std::hash<T>{}(a)` compiles and its result is
-  /// convertible to `std::size_t`. From https://en.cppreference.com/w/cpp/language/constraints.
-  template <typename T>
-  concept hashable = requires(T a) {
+/// @brief Declaration of the concept `Hashable`, which is satisfied by any type `T` such that for
+/// values `a` of type `T`, the expression `std::hash<T>{}(a)` compiles and its result is
+/// convertible to `std::size_t`. From https://en.cppreference.com/w/cpp/language/constraints.
+template <typename T>
+concept hashable = requires(T a) {
     { std::hash<T>{}(a) } -> std::convertible_to<std::size_t>;
-  };
+};
 
-  template <typename T>
-  concept iterable = std::ranges::forward_range<T>;
+template <typename T>
+concept iterable = std::ranges::forward_range<T>;
 
-  template <typename T>
-  concept arithmetic = std::is_floating_point_v<T>;
+template <typename T>
+concept arithmetic = std::is_floating_point_v<T>;
 
-  using bitvec = std::vector<bool>;
+using bitvec = std::vector<bool>;
 
-  /**
-   * @brief A zero-dependency bloom filter implementation. The data structure provides efficient
-   * data storage and lookup. It is important to note that due to the probabilistic nature of the
-   * data structure, there is a chance for false positive results. In other words, after inserting
-   * data into the bloom filter, a lookup can either tell that the data is present with some
-   * probability of the false positive outcome or tell that the data is definitely not present in
-   * the data structure.
-   */
-  struct BloomFilter {
+/**
+ * @brief A zero-dependency bloom filter implementation. The data structure provides efficient
+ * data storage and lookup. It is important to note that due to the probabilistic nature of the
+ * data structure, there is a chance for false positive results. In other words, after inserting
+ * data into the bloom filter, a lookup can either tell that the data is present with some
+ * probability of the false positive outcome or tell that the data is definitely not present in
+ * the data structure.
+ */
+struct BloomFilter {
     // Number of bits in the bit vector.
     std::size_t bits;
     // Number of hash functions.
@@ -72,16 +72,16 @@ namespace bf {
      * @param eps False positive probability.
      */
     explicit constexpr BloomFilter(const std::uint64_t elems, const arithmetic auto eps) {
-      if (elems <= 0) {
-        throw std::domain_error("Number of elements must be greater than zero.");
-      }
-      if (eps < 0 || eps > 1) {
-        throw std::domain_error("False positive probability must be between zero and one.");
-      }
+        if (elems <= 0) {
+            throw std::domain_error("Number of elements must be greater than zero.");
+        }
+        if (eps < 0 || eps > 1) {
+            throw std::domain_error("False positive probability must be between zero and one.");
+        }
 
-      bits = -std::ceil(elems * std::log(eps) / std::pow(std::log(2), 2));
-      hash_fns = std::ceil(static_cast<float>(bits) / static_cast<float>(elems) * std::log(2));
-      bvec = std::vector(bits, false);
+        bits = -std::ceil(elems * std::log(eps) / std::pow(std::log(2), 2));
+        hash_fns = std::ceil(static_cast<float>(bits) / static_cast<float>(elems) * std::log(2));
+        bvec = std::vector(bits, false);
     }
 
     /**
@@ -90,10 +90,10 @@ namespace bf {
      */
     template <hashable T>
     constexpr void insert(T data) noexcept {
-      auto hash = std::hash<T>{};
-      for (std::size_t idx = 0; idx < hash_fns; idx++) {
-        bvec[(hash(data) + idx << 2) % bits] = true;
-      }
+        auto hash = std::hash<T>{};
+        for (std::size_t idx = 0; idx < hash_fns; idx++) {
+            bvec[(hash(data) + idx << 2) % bits] = true;
+        }
     }
 
     /**
@@ -101,9 +101,9 @@ namespace bf {
      * @param it An iterable containing elements to be inserted into the bloom filter.
      */
     constexpr void insert_many(iterable auto it) noexcept {
-      for (const auto& el : it) {
-        insert(el);
-      }
+        for (const auto& el : it) {
+            insert(el);
+        }
     }
 
     /**
@@ -113,13 +113,13 @@ namespace bf {
      */
     template <hashable T>
     [[nodiscard]] constexpr auto search(T data) const noexcept {
-      auto hash = std::hash<T>{};
-      for (std::size_t idx = 0; idx < hash_fns; idx++) {
-        if (!bvec[(hash(data) + idx << 2) % bits]) {
-          return false;
+        auto hash = std::hash<T>{};
+        for (std::size_t idx = 0; idx < hash_fns; idx++) {
+            if (!bvec[(hash(data) + idx << 2) % bits]) {
+                return false;
+            }
         }
-      }
-      return true;
+        return true;
     }
 
     /**
@@ -128,20 +128,20 @@ namespace bf {
      * @return A boolean value specifying whether the input was present or not.
      */
     [[nodiscard]] auto search_many(iterable auto it) const noexcept {
-      bitvec res{};
-      for (const auto& el : it) {
-        res.push_back(search(el));
-      }
-      return res;
+        bitvec res{};
+        for (const auto& el : it) {
+            res.push_back(search(el));
+        }
+        return res;
     }
 
     /**
      * @brief Clears the bloom filter.
      */
     auto clear() noexcept {
-      bvec = std::vector(bits, false);
+        bvec = std::vector(bits, false);
     }
-  };
+};
 
 }  // namespace bf
 
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,121 +1,121 @@
-#include <catch2/catch.hpp>
+#include <catch2/catch_all.hpp>
 
 #include "../include/bf.hpp"
 
 TEST_CASE("Insert and search strings", "[insert][search][string]") {
-  auto bf = bf::BloomFilter{6, 1e-2};
+    auto bf = bf::BloomFilter{6, 1e-2};
 
-  bf.insert("");
-  bf.insert("hello");
-  bf.insert("world");
-  bf.insert("I");
-  bf.insert("am");
-  bf.insert("here");
+    bf.insert("");
+    bf.insert("hello");
+    bf.insert("world");
+    bf.insert("I");
+    bf.insert("am");
+    bf.insert("here");
 
-  REQUIRE(bf.search(""));
-  REQUIRE(bf.search("hello"));
-  REQUIRE(bf.search("world"));
-  REQUIRE(bf.search("I"));
-  REQUIRE(bf.search("am"));
-  REQUIRE(bf.search("here"));
+    REQUIRE(bf.search(""));
+    REQUIRE(bf.search("hello"));
+    REQUIRE(bf.search("world"));
+    REQUIRE(bf.search("I"));
+    REQUIRE(bf.search("am"));
+    REQUIRE(bf.search("here"));
 
-  bf.clear();
+    bf.clear();
 
-  REQUIRE(!bf.search(""));
-  REQUIRE(!bf.search("hello"));
-  REQUIRE(!bf.search("world"));
-  REQUIRE(!bf.search("I"));
-  REQUIRE(!bf.search("am"));
-  REQUIRE(!bf.search("here"));
+    REQUIRE(!bf.search(""));
+    REQUIRE(!bf.search("hello"));
+    REQUIRE(!bf.search("world"));
+    REQUIRE(!bf.search("I"));
+    REQUIRE(!bf.search("am"));
+    REQUIRE(!bf.search("here"));
 }
 
 TEST_CASE("Insert and search numbers", "[insert][search][number]") {
-  auto bf = bf::BloomFilter{12, 1e-1};
+    auto bf = bf::BloomFilter{12, 1e-1};
 
-  bf.insert(1.2);
-  bf.insert(2.8);
-  bf.insert(10.0);
-  bf.insert(1);
-  bf.insert(2);
-  bf.insert(8);
-  bf.insert(32);
+    bf.insert(1.2);
+    bf.insert(2.8);
+    bf.insert(10.0);
+    bf.insert(1);
+    bf.insert(2);
+    bf.insert(8);
+    bf.insert(32);
 
-  REQUIRE(bf.search(10.0));
-  REQUIRE(bf.search(1.2));
-  REQUIRE(bf.search(10.0));
-  REQUIRE(bf.search(1));
-  REQUIRE(bf.search(2));
-  REQUIRE(bf.search(8));
-  REQUIRE(bf.search(32));
+    REQUIRE(bf.search(10.0));
+    REQUIRE(bf.search(1.2));
+    REQUIRE(bf.search(10.0));
+    REQUIRE(bf.search(1));
+    REQUIRE(bf.search(2));
+    REQUIRE(bf.search(8));
+    REQUIRE(bf.search(32));
 
-  REQUIRE(!bf.search(10));
-  REQUIRE(!bf.search(3.0));
-  REQUIRE(!bf.search(3));
-  REQUIRE(!bf.search(1.6));
-  REQUIRE(!bf.search(4));
-  REQUIRE(!bf.search(16));
-  REQUIRE(!bf.search(64));
-  REQUIRE(!bf.search(128));
-  REQUIRE(!bf.search(256));
+    REQUIRE(!bf.search(10));
+    REQUIRE(!bf.search(3.0));
+    REQUIRE(!bf.search(3));
+    REQUIRE(!bf.search(1.6));
+    REQUIRE(!bf.search(4));
+    REQUIRE(!bf.search(16));
+    REQUIRE(!bf.search(64));
+    REQUIRE(!bf.search(128));
+    REQUIRE(!bf.search(256));
 }
 
 TEST_CASE("Insert and search strings (substrings and concatenations)", "[insert][search][string]") {
-  auto bf = bf::BloomFilter{3, 1e-2};
+    auto bf = bf::BloomFilter{3, 1e-2};
 
-  bf.insert("me");
-  bf.insert("yo");
-  bf.insert("you");
+    bf.insert("me");
+    bf.insert("yo");
+    bf.insert("you");
 
-  REQUIRE(bf.search("me"));
-  REQUIRE(bf.search("yo"));
-  REQUIRE(bf.search("you"));
+    REQUIRE(bf.search("me"));
+    REQUIRE(bf.search("yo"));
+    REQUIRE(bf.search("you"));
 
-  bf.clear();
+    bf.clear();
 
-  REQUIRE(!bf.search(""));
-  REQUIRE(!bf.search("m"));
-  REQUIRE(!bf.search("e"));
-  REQUIRE(!bf.search("o"));
-  REQUIRE(!bf.search("y"));
-  REQUIRE(!bf.search("u"));
+    REQUIRE(!bf.search(""));
+    REQUIRE(!bf.search("m"));
+    REQUIRE(!bf.search("e"));
+    REQUIRE(!bf.search("o"));
+    REQUIRE(!bf.search("y"));
+    REQUIRE(!bf.search("u"));
 
-  REQUIRE(!bf.search("meyo"));
-  REQUIRE(!bf.search("yome"));
+    REQUIRE(!bf.search("meyo"));
+    REQUIRE(!bf.search("yome"));
 
-  REQUIRE(!bf.search("meyou"));
-  REQUIRE(!bf.search("youme"));
-  REQUIRE(!bf.search("yoyou"));
-  REQUIRE(!bf.search("youyo"));
+    REQUIRE(!bf.search("meyou"));
+    REQUIRE(!bf.search("youme"));
+    REQUIRE(!bf.search("yoyou"));
+    REQUIRE(!bf.search("youyo"));
 
-  REQUIRE(!bf.search("meyoyou"));
-  REQUIRE(!bf.search("meyouyo"));
-  REQUIRE(!bf.search("yomeyou"));
-  REQUIRE(!bf.search("yoyoume"));
-  REQUIRE(!bf.search("youmeyo"));
-  REQUIRE(!bf.search("youyome"));
+    REQUIRE(!bf.search("meyoyou"));
+    REQUIRE(!bf.search("meyouyo"));
+    REQUIRE(!bf.search("yomeyou"));
+    REQUIRE(!bf.search("yoyoume"));
+    REQUIRE(!bf.search("youmeyo"));
+    REQUIRE(!bf.search("youyome"));
 }
 
 TEST_CASE("Insert and search strings from a vector", "[insert][search][string]") {
-  auto bf = bf::BloomFilter(5, 1e-3);
-  auto words = std::vector<std::string>{"afopsiv", "coxpz", "pqeacxnvzm", "zm", "acxk"};
+    auto bf = bf::BloomFilter(5, 1e-3);
+    auto words = std::vector<std::string>{"afopsiv", "coxpz", "pqeacxnvzm", "zm", "acxk"};
 
-  for (const auto& word : words) {
-    bf.insert(word);
-    REQUIRE(bf.search(word));
-  }
+    for (const auto& word : words) {
+        bf.insert(word);
+        REQUIRE(bf.search(word));
+    }
 }
 
 TEST_CASE("Insert many and search many", "[insert_many][search_many][int]") {
-  auto bf = bf::BloomFilter(12, 1e-4);
-  auto nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    auto bf = bf::BloomFilter(12, 1e-4);
+    auto nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 
-  bf.insert_many(nums);
-  for (const auto& num : nums) {
-    REQUIRE(bf.search(num));
-  }
+    bf.insert_many(nums);
+    for (const auto& num : nums) {
+        REQUIRE(bf.search(num));
+    }
 
-  auto vals = bf.search_many(nums);
-  for (const auto& val : vals) {
-    REQUIRE(val);
-  }
+    auto vals = bf.search_many(nums);
+    for (const auto& val : vals) {
+        REQUIRE(val);
+    }
 }
